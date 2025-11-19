0b42022e14f2bdad3fdd776d85a83b597db5a12c
subhadip07 <subhadipbag18@outlook.com>
2025-11-19 22:06:36 +0530

    add: Step-3 & 4: Transform and Load

--- ETL_pipeline/dags/etl.py
+++ ETL_pipeline/dags/etl.py
@@ -44,3 +44,36 @@ with DAG(
         data={"api_key": "{{ conn.nasa_api.extra_dejson.api_key }}"}, ## Use the API key from connection
         response_filter=lambda response:response.json(), ## Convert response to json
     )
+
+    ## Step 3: Transform the data (Pick the information that we need to save)
+    @task
+    def transform_apod_data(response):
+        apod_data={
+            'title': response.get('title', ''),
+            'explanation': response.get('explanation', ''),
+            'url': response.get('url', ''),
+            'date': response.get('date', ''),
+            'media_type': response.get('media_type', '')
+        }
+        return apod_data
+    
+    ## Step 4: Load the data into Postgres
+    @task
+    def load_data_to_postgres(apod_data):
+        ## Initialize the Postgres hook
+        postgres_hook = PostgresHook(postgres_conn_id='my_postgres_connection')
+
+        ## Define the SQL insert query
+        insert_query = """
+        INSERT INTO apod_data (title, explanation, url, date, media_type)
+        VALUES (%s, %s, %s, %s, %s);
+        """
+
+        ## Execute the SQL Query
+        postgres_hook.run(insert_query, parameters=(
+            apod_data['title'],
+            apod_data['explanation'],
+            apod_data['url'],
+            apod_data['date'],
+            apod_data['media_type']
+        ))
